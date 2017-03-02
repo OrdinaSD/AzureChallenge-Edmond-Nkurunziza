b015b664f982940b6f70d8a21a804fc034e67a94
Edmond Nkurunziza <edmond.nkurunziza@ordina.nl>
2017-03-02 19:56:36 +0000

    Edmonk first commit.

---
+++ EdmondSocialNetwork/EdmondSocialNetwork.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EdmondSocialNetwork.Web.Startup))]
+namespace EdmondSocialNetwork.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
